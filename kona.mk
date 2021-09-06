496e825cdf23df33c7949b035045d6e59a5b714b
Cristian Pardo <cristian.pardo@externos.bci.cl>
2021-09-06 00:32:49 -0300

    update variant info

--- kona.mk
+++ kona.mk
@@ -250,10 +250,8 @@ PRODUCT_PACKAGES += \
     android.hardware.lights-service.xiaomi_kona
 
 # LiveDisplay
-ifeq ($(findstring Plus, $(CUSTOM_VERSION)),)
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service-sdm
-endif
 
 # Media
 PRODUCT_PACKAGES += \
